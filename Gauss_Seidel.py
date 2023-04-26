1f0984ad7235a8574f1419120a6c0d6af32854eb
Piyush R. Maharana <61222720+catastropiyush@users.noreply.github.com>
2023-04-26 13:20:02 +0530

    Update Gauss_Seidel.py

--- Gauss_Seidel.py
+++ Gauss_Seidel.py
@@ -1,14 +1,29 @@
 #Gauss Seidel Iteration 
 #Last update:Piyush
-N=5 #number of iterations
+import numpy as np
+import matplotlib.pyplot as plt
+N=10 #number of iterations
 # 27x+6y-z=54
 # 6x+15y+2z=72
 # x+y+54z=110
-x=0;y=0;z=0
-for i in range(N):
-  x = (1/27)*(54-6*y+z)
-  y = (1/15)*(72-6*x-2*z)
-  z = (1/54)*(110-x-y)
-print('x',x)
-print('y',y)
-print('z',z)
+a=[0]*N
+b=[0]*N
+c=[0]*N
+
+a[0] = 10.0  
+b[0] = 2.0  
+c[0] = 3.0
+for i in range(1,N):
+  a[i] = (1/27)*(54-6*b[i-1]+c[i-1])
+  b[i] = (1/15)*(72-6*a[i]-2*c[i-1])
+  c[i] = (1/54)*(110-a[i]-b[i])
+
+plt.plot(a, label=r'$x_{1}$',linewidth=2.0,color='deeppink')
+plt.plot(b, label=r'$x_{2}$',linewidth=2.0,color='orchid')
+plt.plot(c, label=r'$x_{3}$',linewidth=2.0,color='cornflowerblue')
+plt.legend()
+plt.grid(color='lightgrey', linestyle='-', linewidth=0.5)
+
+print('x',a[-1])
+print('y',b[-1])
+print('z',c[-1])
