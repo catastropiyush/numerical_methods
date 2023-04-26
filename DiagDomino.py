fb46450e706e92eeb9865eb42f436822b3b13c44
Piyush R. Maharana <61222720+catastropiyush@users.noreply.github.com>
2023-04-26 14:31:43 +0530

    Create DiagDomino.py

---
+++ DiagDomino.py
@@ -0,0 +1,13 @@
+#Diagonal Dominance checker
+import numpy as np 
+import matplotlib.pyplot as plt
+A = np.array([[6,-2,10],[1,-5,2],[-1,2,8]])
+tick = [0]*len(A)
+N=len(np.sum(np.abs(A), axis=1))
+sum_row=np.sum(np.abs(A), axis=1)
+for i in range(0,N):
+  if(np.abs(A[i][i])>(sum_row[i]-np.abs(A[i][i]))):
+    tick[i]='Yes'
+  else:
+    tick[i]='No'
+print(tick) 
